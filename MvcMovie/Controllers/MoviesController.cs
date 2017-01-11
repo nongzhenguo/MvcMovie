1fb8f68e249c58176a3484760ce0ee1bfaa9cd87
nongzhenguo <957608954@qq.com>
2017-01-11 11:50:42 +0800

    修改

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -13,27 +13,7 @@ namespace MvcMovie.Controllers
     public class MoviesController : Controller
     {
         private MovieDBContext db = new MovieDBContext();
-
-        // GET: Movies
-        //public ActionResult Index(string searchString)
-
-        //{
-        //    var movies = from m in db.Movies
-
-        //                 select m;
-
-        //    if (!String.IsNullOrEmpty(searchString))
-
-        //    {
-
-        //        movies = movies.Where(s => s.Title.Contains(searchString));
-
-        //    }
-
-        //    return View(movies);
-
-        //}
-
+        
         public ActionResult Index(string movieGenre, string searchString)
 
         {
